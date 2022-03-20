d83ceed0b5654b7cf4f4f09ce74c7f0331b070fd
StickmanXD <44358476+StickmanXD@users.noreply.github.com>
2022-03-20 18:55:36 +0900

    Add files via upload

---
+++ 20220317/aeb_publisher.cpp
@@ -0,0 +1,34 @@
+#include "ros/ros.h"
+#include "std_msgs/Int8.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/Range.h"
+#include "geometry_msgs/Twist.h"
+
+std_msgs::Bool flag_AEB;
+
+void UltraCallBack(const sensor_msgs::Range::ConstPtr& msg) {
+  if(msg->range <= 1.0) {
+		flag_AEB.data = true;
+  }
+  else {
+		flag_AEB.data = false;
+  }
+}
+
+int main(int argc, char**argv) {
+	
+	ros::init(argc, argv, "aeb_publisher");
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("range", 1000, UltraCallBack);
+	ros::Publisher pub1 = n.advertise<std_msgs::Bool>("bool1", 1000);
+	ros::Rate loop_rate(1);
+	
+	while(ros::ok()) {
+		pub1.publish(flag_AEB);   
+		loop_rate.sleep();
+		ros::spinOnce();
+	}
+	return 0;
+}
